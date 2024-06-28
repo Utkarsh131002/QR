f96fc459975b23d16a8147227604fe7d3880b3dc
Utkarsh Patkar <144775807+Utkarsh131002@users.noreply.github.com>
2024-06-28 23:29:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const wrapper = document.querySelector(".wrapper")
+qrInput = document.querySelector(".form input")
+generateBtn = document.querySelector(".form button")
+qrImg = document.querySelector(".qr-code img")
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    })
+    
+})
+
